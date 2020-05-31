49fdbb0901c45933c0336975d0e0527c6c13144a
manamante <66200318+manamante@users.noreply.github.com>
2020-05-31 15:20:24 +0530

    __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -122,7 +122,7 @@ def start(bot: Bot, update: Update, args: List[str]):
             send_start(bot, update)
     else:
         try:
-            update.effective_message.reply_text("Hey there! I'm alive :3")
+            update.effective_message.reply_text("Hey there! I'm alive. PM Me If You Have Any Questions!")
         except:
             print("Nut")
 
@@ -138,11 +138,11 @@ def send_start(bot, update):
     text = "Hey there! My name is Haruka Aya - I'm here to help you manage your groups!\n\
 Click Help button to find out more about how to use me to my full potential.\n\n"
 
-    text += "Join [Haruka Aya Group](https://t.me/HarukaAyaGroup) ( @HarukaAyaGroup ) if you need any support or help\n\n\
-Follow [Haruka Aya](https://t.me/HarukaAya) ( @HarukaAya ) if you want to keep up with the news, updates and bot downtime!\n\n\
-Made with love by @RealAkito\n\nWant to add me to your group? [Click here!](t.me/HarukaAyaBot?startgroup=true)"
+    text += "Join [Haruka Aya Group](https://t.me/Sabrina_RobotChat) if you need any support or help\n\n\
+Follow [Haruka Aya](https://t.me/Hackers_Squad) if you want to keep up with the news, updates and bot downtime!\n\n\
+Made with love by @RealAkito\n\nWant to add me to your group? [Click here!](t.me/Haruka_Aya_Bot?startgroup=true)"
 
-    keyboard = [[InlineKeyboardButton(text="📢 Support Group", url="https://t.me/HarukaAyaGroup")]]
+    keyboard = [[InlineKeyboardButton(text="Founder", url="https://t.me/Unknown_Hacker_X")]]
     keyboard += [[InlineKeyboardButton(text="🛠 Control panel", callback_data="cntrl_panel_M")]]
     keyboard += [[InlineKeyboardButton(text="🇺🇸 Language", callback_data="set_lang_"), 
         InlineKeyboardButton(text="❔ Help", callback_data="help_back")]]
